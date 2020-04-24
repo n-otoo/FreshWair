7e082a48a674526923ff6c09aaeb807ee33e53c2
n-otoo <nyameye.otoo@outlook.com>
2020-04-24 01:06:13 +0100

    Sensor now pushing data to server and adding to buffer when fail

--- testcode.py
+++ testcode.py
@@ -1,41 +1,45 @@
-import Adafruit_DHT
+#import Adafruit_DHT
  
 import json
 import time
 import requests
 from sds011 import *
+from buffermanager import *
 
 # Set sensor type : Options are DHT11,DHT22 or AM2302
-sensor=Adafruit_DHT.DHT11
+#sensor=Adafruit_DHT.DHT11
  
 # Set GPIO sensor is connected to
 gpio=3
 
 # Air Quality Sensor
-PMSensor = SDS011("/dev/ttyUSB0")
-sensor.sleep(sleep=true)
+PMsensor = SDS011("/dev/ttyUSB0")
+PMsensor.sleep(sleep=False)
 time.sleep(10)
-pm25, pm10 = sensor.query()
-print("PM2.5 is :" + pm25 + " PM10 :" + pm10)
+pm25, pm10 = PMsensor.query()
+print("PM2.5 is :" + str( pm25) + " PM10 :" + str(pm10))
 
 # Set the URL For posting
 #url = "http://192.168.43.136:9090/scarf/env/pm"
-url = "https://kite.com/fakepagewitherror/error"
+url = "http://192.168.0.103:8080/scarf/env/pm"
 # Use read_retry method. This will retry up to 15 times to
 # get a sensor reading (waiting 2 seconds between each retry).
 for x in range(5):
-  humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-  data = {"dateTime":10090, "pm25":int(humidity), "pm10":int(temperature), "lat":0, "lon":0}
- 
+  pm25, pm10 = PMsensor.query()
+  time.sleep(2)
+  data = {"dateTime":time.time(), "pm25":int(pm25), "pm10":int(pm10), "lat":57.194, "lon":-2.118}
+  print(str(data))
   # Reading the DHT11 is very sensitive to timings and occasionally
   # the Pi might fail to get a valid reading. So check if readings are valid.
-  if humidity is not None and temperature is not None:
+  if pm25 is not None and pm10 is not None:
     try:
-        resp = requests.get(url)
+        resp = requests.post(url, data=data)
     except Exception as e:
         print(e)
+        check_for_buffer()
+        write_to_buffer(data)
         try:
-            resp.raise_for_status()
+            e.raise_for_status()
         except requests.exceptions.HTTPError as error:
             print(error)
     #resp = requests.post(url, data = data)
