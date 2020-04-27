a312ad1da159d32a7359f769721c296ae3a24c8a
n-otoo <nyameye.otoo@outlook.com>
2020-04-27 22:56:59 +0100

    added gps manager to the testcode.py file, complete as of now

--- testcode.py
+++ testcode.py
@@ -5,6 +5,7 @@ import time
 import requests
 from sds011 import *
 from buffermanager import *
+from gpsmanager import *
  
 # Air Quality Sensor
 PMsensor = SDS011("/dev/ttyUSB0")
@@ -14,7 +15,7 @@ time.sleep(10)
 # print("PM2.5 is :" + str( pm25) + " PM10 :" + str(pm10))
 
 # Set the URL For posting
-url = "http://192.168.0.103:8080/scarf/env/pm"
+url = "https://192.168.0.103:8443/scarf/env/pm"
 
 check_for_buffer()
 didSucceedInSendingBuffer = True
@@ -31,15 +32,16 @@ if didSucceedInSendingBuffer:
         os.remove("buffer.txt")
  
 # get a sensor reading (waiting 2 seconds between each retry).
-for x in range(5):
+for x in range(3):
   pm25, pm10 = PMsensor.query()
+  location = get_location()
   time.sleep(2)
-  data = {"dateTime":int(round(time.time()* 1000)), "pm25":int(pm25), "pm10":int(pm10), "lat":57.194, "lon":-2.118}
+  data = {"dateTime":int(round(time.time()* 1000)), "pm25":int(pm25), "pm10":int(pm10), "lat":location["lat"], "lon":location["lon"]}
   print(str(data) + " " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
   # the Pi might fail to get a valid reading. So check if readings are valid.
   if pm25 is not None and pm10 is not None:
     try:
-        resp = requests.post(url, data=data)
+        resp = requests.post(url, data=data, verify=False)
         print(resp.content)
     except Exception as e:
         print(e)
